b9ababf52f12e4fb92b7cc7ef881cd3572c514eb
MH Shuvo <mhshuvoalways@gmail.com>
2024-10-31 11:38:52 +0600

    feat: dynamic avaiablitiy

--- app/api/booking/[date]/route.js
+++ app/api/booking/[date]/route.js
@@ -2,6 +2,7 @@ import { dbConnect } from "@/db/mongodb";
 import Booking from "@/models/Booking";
 import serverError from "@/services/serverError";
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export const POST = async (request, { params }) => {
   try {
